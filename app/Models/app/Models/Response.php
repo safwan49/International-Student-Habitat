6b706f3ad6ac485f018a87968670689b6685b796
Raida <fauzia.abida.raida@g.bracu.ac.bd>
2026-04-19 22:04:58 +0600

    Create Response.php

---
+++ app/Models/app/Models/Response.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    protected $fillable = [
+        'question_id',
+        'user_id',
+        'content',
+        'is_most_helpful'
+    ];
+}
